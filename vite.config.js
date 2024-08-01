d203a3847619e2d79402e7c4e73bfd7da11116cd
jerry karinga <kp.rejy1972@gmail.com>
2024-08-01 16:53:53 +0530

    3rd commit  size increasd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'] // Example of manual chunking
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Adjust the chunk size warning limit
+  }
+});
